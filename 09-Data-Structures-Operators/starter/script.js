95c9049dba438fe879ad6689bd51c17593ef8a20
Matt Radich <mattradich2509@gmail.com>
2022-11-21 15:35:18 +1300

    Destructing Arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,11 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+  //function to order food, return 2 values
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +32,59 @@ const restaurant = {
     },
   },
 };
+
+//Destructing Arrays
+const arr = [2,3,4]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+
+const [x,y,z] = arr
+console.log(x,y,z);//console shows 2 3 4
+console.log(arr);//origional array is not effected, we jsut unpack it
+//
+
+//taking data from restaurant , access categories array, destructure && display
+const [first, second] = restaurant.categories
+console.log(first,second);
+
+//skip second with empty value, only displays first && third
+const [first1, ,third1] = restaurant.categories
+console.log(first1,third1);
+
+//lets switch main & secondary categories
+let [main, secondary] = restaurant.categories
+console.log(main, secondary);
+
+//Switching variables
+// const temp = main
+// main = secondary
+// secondary = temp
+// console.log(main, secondary);
+
+//instead of above we destructure
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+//Recieve 2 return values from a function
+//number [2] from the starter menu &&
+//number [0] from the main menu ==
+//Garlic Bread && Pizza
+const [starter, mainCourse] = restaurant.order(2,0)
+console.log(starter, mainCourse);
+
+//Nested Array destructure
+const nested = [2,4, [5,6]];
+// const [i, ,j] = nested
+// console.log(i,j); returns [5,6]
+
+const [i, , [j, k]] = nested
+console.log(i, j ,k); //returns 2, 5 ,6
+
+//Default Values
+const [p, q ,r] = [8, 9]
+console.log(p, q, r); //returns 8, 9, undefined
+
+const [m = 1, n = 1, v = 1] = [8, 9]
+console.log(m, n , v); //returns 8, 9 , 1
